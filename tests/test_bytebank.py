f9292d22beba220499b1a534780a78dab4b3c539
Lucas Vieira Goncalves <20221461@sarah.br>
2024-04-03 15:30:08 -0300

    primeiros testes

---
+++ tests/test_bytebank.py
@@ -0,0 +1,26 @@
+from codigo.bytebank import Funcionario
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_200_deve_retornar_22(self):
+        #Given(contexto)
+        entrada = '13/03/200'
+        esperado = 22
+
+        funcionario_test = Funcionario('Teste', entrada, 3400)
+
+        #Whewn(Ação)
+        resultado = funcionario_test.idade()
+
+
+        #Then(Desfecho)
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_lucas_vieira_deve_retornar_vieira(self):
+        entrada = 'Lucas Vieira '
+        esperado = 'Vieira'
+
+        lucasVieira = Funcionario(entrada, '14/05/2004', 3400)
+
+        resultado = lucasVieira.sobrenome()
+
+        assert resultado == esperado
